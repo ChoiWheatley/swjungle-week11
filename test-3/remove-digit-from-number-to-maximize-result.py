89484afc812bdf3259976e91822fdf08449bcddb
ChoiWheatley <chltmdgus604@naver.com>
2023-10-30 21:44:34 +0900

    2259 🆗 인접 원소만 보는 풀이법

--- test-3/remove-digit-from-number-to-maximize-result.py
+++ test-3/remove-digit-from-number-to-maximize-result.py
@@ -30,6 +30,7 @@ class Solution2:
     digit보다 큰 숫자가 digit 뒤에 나타나는 경우가 최대.
     [1,3,3,1,7,3,2], digit = 3 을 예시로 들면, 처음 두 3을 제거하게 되면
     131732가 되고, 세번째 3을 제거하게 되면 133172가 되므로 전자가 더 크게 된다.
+    24ms 16.17MB
     """
 
     def removeDigit(self, number: str, digit: str) -> str:
@@ -46,6 +47,27 @@ class Solution2:
         return max(maxstr, str_remove_at(number, found))
 
 
+class Solution3:
+    """
+    digit 바로 뒤에 있는 원소만 보면 되는 것이었다... 🙊
+    37ms 16.23MB
+    """
+
+    def removeDigit(self, number: str, digit: str) -> str:
+        found = -1
+        for i in range(len(number) - 1):
+            if number[i] == digit:
+                found = i
+            if number[i] == digit and digit < number[i + 1]:
+                # 바로 오른쪽 원소하고만 대소비교한다.
+                return str_remove_at(number, i)
+
+        if number[-1] == digit:
+            found = len(number) - 1
+
+        return str_remove_at(number, found)
+
+
 if __name__ == "__main__":
     testcases = [
         # (number, digit, correct)
@@ -59,7 +81,7 @@ if __name__ == "__main__":
         ("43553327", "3", "4553327"),
         ("3619552534", "5", "361955234"),
     ]
-    s = Solution2()
+    s = Solution3()
 
     for number, digit, correct in testcases:
         answer = s.removeDigit(number, digit)
